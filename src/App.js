7b17e13aa9e48889a7e1c7e6dc5e522032a8c25a
Happiness Balogun <happinessblgn0@gmail.com>
2020-10-23 03:09:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,120 @@
+import React from "react";
+import "./App.css";
+
+// function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchBox: "",
+      items: [],
+    };
+    this.getSearchResults = this.getSearchResults.bind(this);
+  }
+
+  async getSearchResults(e) {
+    // const req = await fetch("http://localhost:3000/Books.json");
+    const req = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=isbn:${this.state.searchBox}`
+    );
+
+    const res = await req.json();
+    this.setState({ items: res.items, searchBox: "" });
+    console.log("items", this.state.searchBox);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <div className="form">
+          <input
+            type="number"
+            value={this.state.searchBox}
+            id="search-box"
+            placeholder="Enter an ISBN to search for a book. An example is '0747532699'"
+            onChange={(e) => {
+              this.setState({ searchBox: e.target.value });
+            }}
+          />
+          <button onClick={this.getSearchResults}>Search</button>
+        </div> */}
+
+        <form class="search-container form">
+          <input
+            type="text"
+            value={this.state.searchBox}
+            id="search-bar"
+            placeholder="0747532699"
+            onChange={(e) => {
+              this.setState({ searchBox: e.target.value });
+            }}
+          />
+          <a href="#">
+            <img
+              onClick={this.getSearchResults}
+              class="search-icon"
+              src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"
+            />
+          </a>
+        </form>
+
+        {this.state.items.map((item, index) => {
+          if (index === 0) {
+            return (
+              <div key={item.id} className="card">
+                <div className="wrapper">
+                  <div className="card-image">
+                    <img
+                      src={item.volumeInfo.imageLinks.thumbnail}
+                      alt="Book preview"
+                    />
+                  </div>
+                  <div className="card-info">
+                    <h2>{item.volumeInfo.title}</h2>
+                    <h5>
+                      {item.volumeInfo.subtitle ? item.volumeInfo.subtitle : ""}
+                    </h5>
+                    <p className="author">
+                      {item.volumeInfo.authors === undefined
+                        ? item.volumeInfo.publisher
+                        : item.volumeInfo.authors.map((author) => {
+                            return <span key={author}>{author} | </span>;
+                          })}
+                    </p>
+                    <span>{item.volumeInfo.publisher} | </span>
+                    <span
+                      className={
+                        item.volumeInfo.pageCount === "" ? "show" : "hide"
+                      }
+                    >
+                      {item.volumeInfo.publishedDate} |
+                    </span>
+                    <span>{item.volumeInfo.categories} | </span>
+                    <span
+                      className={
+                        item.volumeInfo.pageCount === "" ? "show" : "hide"
+                      }
+                    >
+                      {item.volumeInfo.pageCount}
+                      pages
+                    </span>
+                    <p className="desc">{item.volumeInfo.description}</p>
+                    <a
+                      href={item.volumeInfo.previewLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Get more info
+                    </a>
+                  </div>
+                </div>
+              </div>
+            );
+          }
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
